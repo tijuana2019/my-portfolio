e6ed67b116f5415030c030e5cc90f93035ef6a13
Jo <jo_serendipity@icloud.com>
2019-10-07 19:53:27 +0100

    updated js code

--- Scripts.js
+++ Scripts.js
@@ -5,7 +5,7 @@
 
     function submitForm() {
 
-        let userInput = document.getElementByClassName.("userInput");
+        
         document.getElementById("submit").addEventListener("click", alert("The details you entered are: " + userInput);
             
 
