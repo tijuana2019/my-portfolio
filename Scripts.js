8b8a2b04c9190ad03547e2aa5c0006d9b9f03ff2
Jo <jo_serendipity@icloud.com>
2019-10-07 20:14:25 +0100

    updated js file

--- Scripts.js
+++ Scripts.js
@@ -7,6 +7,8 @@
         let userName = document.getElementById("name");
         let userEmail = document.getElementById("mail");
         let userMsg = document.getElementById("msg");   
-        document.getElementById("submit").addEventListener("click", alert("Thanks! The details you entered are: " + userName + userEmail + userMsg);
         }
+    function formAlert {
+        document.getElementById("submit").addEventListener("click", alert("Thanks! The details you entered are: " + userName + userEmail + userMsg);
+    }
 </script>
