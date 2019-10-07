847ece21935aa5d6b65111b51a0d8cb61043c655
Jo <jo_serendipity@icloud.com>
2019-10-07 19:30:53 +0100

    updated html to include clear button and added JS file

---
+++ Scripts.js
@@ -0,0 +1,10 @@
+<script>
+    function resetForm() {
+        document.getElementById("reset").reset();
+    }
+
+    function submitForm() {
+        document.getElementById("submit").releasePointerCapture()
+    }
+
+</script>
