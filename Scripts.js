79723236d5cd84ee8f1b4a5b917ebae0d9253a38
Jo <jo_serendipity@icloud.com>
2019-10-07 20:35:34 +0100

    updated event listener

--- Scripts.js
+++ Scripts.js
@@ -1,14 +1,12 @@
 <script>
     
-    let userName = document.getElementById("name");
-    let userEmail = document.getElementById("mail");
-    let userMsg = document.getElementById("msg");   
-    
     function resetForm() {
         document.getElementById("reset").addEventListener("click", alert());
     }  
         
     function formAlert {
-        document.getElementById("submit").addEventListener("click", alert("Thanks! The details you entered are: " + userName + userEmail + userMsg);
-    }
+        document.getElementsByClassName("get-input-text").onclick = function() {
+            var inputText = document.getElementById("input-text");
+            alert(inputText.value);
+          }
 </script>
