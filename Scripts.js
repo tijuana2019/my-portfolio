3e515083ab16fe69a04c1d19ba22acd4654d31c0
Jo <jo_serendipity@icloud.com>
2019-10-07 21:41:02 +0100

    updated js

--- Scripts.js
+++ Scripts.js
@@ -4,10 +4,25 @@
         document.getElementById("reset").addEventListener("click", alert());
     }  
         
-    function formAlert {
-        document.getElementById("submit").onclick = function() {
-            var inputText = document.getElementsByClassName("input-text");
-            alert(inputText.value);
-          }
+    function userName() {
+        var name = document.getElementById("name");
+        alert("Your name is: " + name.value);
+}
+
+    function userMail() {
+    var email = document.getElementById("mail");
+    alert("Your email is: " + email.value);
+}
+    function userMessage() {
+    var message = document.getElementById("msg");
+    alert("Your message is: " + message.value);
+}
+
+    var submit = document.getElementById("submit");
+    submit.addEventListener("click", userName);
+    submit.addEventListener("click", userMail);
+    submit.addEventListener("click", userMessage);
 </script>
+
+
 
