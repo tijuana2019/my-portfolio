04e9f6528fa33682523955a12872132cac8171eb
Jo <jo_serendipity@icloud.com>
2019-10-07 20:30:13 +0100

    updated event listeners

--- Scripts.js
+++ Scripts.js
@@ -1,13 +1,13 @@
 <script>
+    
+    let userName = document.getElementById("name");
+    let userEmail = document.getElementById("mail");
+    let userMsg = document.getElementById("msg");   
+    
     function resetForm() {
         document.getElementById("reset").addEventListener("click", alert());
-    }
-
-    function submitForm() {
-        let userName = document.getElementById("name");
-        let userEmail = document.getElementById("mail");
-        let userMsg = document.getElementById("msg");   
-        }
+    }  
+        
     function formAlert {
         document.getElementById("submit").addEventListener("click", alert("Thanks! The details you entered are: " + userName + userEmail + userMsg);
     }
